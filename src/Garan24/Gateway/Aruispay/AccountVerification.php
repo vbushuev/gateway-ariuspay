101860681618c556cef132581af03d9937bd8ff2
vbushuev <v.bushuev.co@gmail.com>
2016-04-26 16:37:57 +0300

    Corrected

---
+++ src/Garan24/Gateway/Aruispay/AccountVerification.php
@@ -0,0 +1,22 @@
+<?php
+namespace Garan24\Gateway\Aruispay;
+use \Garan24\BaseConnector as BaseConnector;
+use \Garan24\Gateway\Aruispay\Exception  as Garan24GatewayAruispayException;
+use \Garan24\Interfaces\ITransaction as ITransaction;
+class AccountVerification extends BaseConnector{
+    public function __construct($data=[]){
+        parent::__construct(["endpoint"=>"1144"]);
+        $this->_request_data = $data;
+        $this->_method = "account-verification";
+    }
+    protected function build(){
+        $this->_request_data["merchant_control"] = $this->_merchant_key;
+        $this->_request_data["control"] = $this->digest(
+            trim($this->_endpoint)
+            .trim($this->_request_data["client_orderid"])
+            .trim($this->_request_data["email"])
+            .trim($this->_request_data["merchant_control"])
+        );
+    }
+};
+?>
